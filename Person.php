bd6b3bce9f150ca942470b735502c93cd5c037ea
Amadeus Delfino <amadeus.cmd@gmail.com>
2020-08-31 16:01:34 -0300

    Initial commit

---
+++ Person.php
@@ -0,0 +1,27 @@
+<?php
+
+class Person
+{
+    protected $name;
+    protected $email;
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+}
---
+++ without_prototype.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'Person.php';
+
+function convertMemoryUsage($size)
+{
+    $unit = array('b', 'kb', 'mb', 'gb', 'tb', 'pb');
+    return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . ' ' . $unit[$i];
+}
+
+function bench(callable $callback, int $times)
+{
+    $executionTimes = [];
+    for ($i = 0; $i < $times; $i++) {
+        $start = microtime(true);
+        $callback();
+        $executionTimes[] = microtime(true) - $start;
+    }
+
+    $time = array_reduce($executionTimes, function ($carry, $item) {
+        $carry += $item;
+        return $carry;
+    });
+    echo $time / count($executionTimes);
+}
+
+bench(function () {
+    for ($i = 0; $i < 1000000; $i++) {
+        $person = new Person();
+        $person->setName(str_shuffle('abcdefghijklmnopq'));
+        $person->setEmail(str_shuffle('abcdefghijklmnopq'));
+        $personList[] = $person;
+    }
+
+}, 100);
+
